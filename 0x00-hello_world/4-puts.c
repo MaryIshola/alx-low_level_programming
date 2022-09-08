d8944061a52f243bb0848da4e2c275bc876f3ad8
MaryIshola <funmilayoishola72@gmail.com>
2022-09-08 12:50:44 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,27 @@
 #include <stdio.h>
 
-/**
- * main - This is the body of my code
+
+
+|/**
  *
- * Return: Must return an integer value that is zero
- */
+ *  * main - This is the body of the code
+ *
+ *   *
+ *
+ *    * Return: Must return an integer value always zero
+ *
+ *     */
+
 int main(void)
+
 {
+
 	char a[] = "\"Programming is like building a multilingual puzzle";
-	put(a);
-	return(0);
-}	
+
+
+
+	puts(a);
+
+	return (0);
+
+}
