532439e3c59156e97f303468c875f326177a5675
MaryIshola <funmilayoishola72@gmail.com>
2022-09-28 10:28:25 -0700

    task 0

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,11 +1,4 @@
-#ifndef MAIN_H
-#define MAIN_H
 
-/**
- * File: holberton.h
- * Desc: Header file containing prototypes for all functions
- *       used in the 0x07-recursion directory.
- */
 
 int _putchar(char c);
 void _puts_recursion(char *s);
@@ -16,6 +9,4 @@ int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int is_palindrome(char *s);
-int wildcmp(char *s1, char *s2);
-
-#endif
+int wildcmp(char *s1, char *s
